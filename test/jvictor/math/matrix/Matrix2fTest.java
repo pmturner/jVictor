b7ec5cd4c080da2949e7238581bafd12a0e7cf25
Ian Burns <iwburns8@gmail.com>
2015-11-16 17:01:20 -0600

    add matrix2ftest.

---
+++ test/jvictor/math/matrix/Matrix2fTest.java
@@ -0,0 +1,93 @@
+package jvictor.math.matrix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Matrix2fTest {
+
+    @Test
+    public void testTransposeCopy() throws Exception {
+
+    }
+
+    @Test
+    public void testTranspose() throws Exception {
+
+    }
+
+    @Test
+    public void testTransformVectorCopy() throws Exception {
+
+    }
+
+    @Test
+    public void testTransformVector() throws Exception {
+
+    }
+
+    @Test
+    public void testSubMatrices() throws Exception {
+
+    }
+
+    @Test
+    public void testSub() throws Exception {
+
+    }
+
+    @Test
+    public void testSetZero() throws Exception {
+
+    }
+
+    @Test
+    public void testSetIdentity() throws Exception {
+
+    }
+
+    @Test
+    public void testNegateCopy() throws Exception {
+
+    }
+
+    @Test
+    public void testNegate() throws Exception {
+
+    }
+
+    @Test
+    public void testMulMatrices() throws Exception {
+
+    }
+
+    @Test
+    public void testMul() throws Exception {
+
+    }
+
+    @Test
+    public void testInvertCopy() throws Exception {
+
+    }
+
+    @Test
+    public void testInvert() throws Exception {
+
+    }
+
+    @Test
+    public void testDeterminant() throws Exception {
+
+    }
+
+    @Test
+    public void testAddMatrices() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+}
